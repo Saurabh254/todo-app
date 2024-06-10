6bcbcb13f1aa77e1be5377514d4ae76eb287da5b
Saurabh <sauravvishwakarma030@gmail.com>
2024-06-10 20:44:53 +0530

    updated database url

--- todo_backend/config.py
+++ todo_backend/config.py
@@ -1,2 +1,4 @@
-DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/mydb"
-TEST_DATABASE_URL = "postgresql://ubuntu:astro_password@20.244.33.238:5432/todo_backend_test"
+DATABASE_URL = "postgresql://ubuntu:astro_password@20.244.33.238:5432/todo_backend"
+TEST_DATABASE_URL = (
+    "postgresql://ubuntu:astro_password@20.244.33.238:5432/todo_backend_test"
+)
