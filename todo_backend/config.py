63cd0eeaff120d3f7ae5056b9133f133337af952
Saurabh <75117608+Saurabh254@users.noreply.github.com>
2024-06-10 11:25:15 +0530

    Update config.py

--- todo_backend/config.py
+++ todo_backend/config.py
@@ -1,3 +1,2 @@
 DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/mydb"
-REDIS_URL = "redis://localhost:6374/0"
-TEST_DATABASE_URL = "postgresql://ubuntu:astro_password@20.244.33.238:5432/todo_backend"
+TEST_DATABASE_URL = "postgresql://ubuntu:astro_password@20.244.33.238:5432/todo_backend_test"
