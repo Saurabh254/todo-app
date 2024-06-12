02f516bb086cf24ee86ddcaf0e796a77f5fe4d9a
ErSamay <samayjain564@gmail.com>
2024-06-12 13:29:14 +0530

    fix the ui changes

--- todo_frontend/src/Components/AddNewTask.tsx
+++ todo_frontend/src/Components/AddNewTask.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { createTask } from "../api";
 import { TaskResponse } from "../type";
@@ -8,42 +8,18 @@ function AddNewTask() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [status, setStatus] = useState("0");
-  const [titleError, setTitleError] = useState("");
-  const [descriptionError, setDescriptionError] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+ 
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const validateInputs = () => {
-    let isValid = true;
-    if (title.length < 3) {
-      setTitleError("Title must be at least 3 characters.");
-      isValid = false;
-    } else {
-      setTitleError("");
-    }
+ 
 
-    if (description.length < 5) {
-      setDescriptionError("Description must be at least 5 characters.");
-      isValid = false;
-    } else {
-      setDescriptionError("");
-    }
-
-    return isValid;
-  };
-
-  useEffect(() => {
-    if (submitted) {
-      validateInputs();
-    }
-  }, [title, description, submitted]);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setSubmitted(true); 
-    if (!validateInputs()) {
-      return;
-    }
+   
+   
+     setIsLoading(true);
 
     try {
       const numericStatus = parseInt(status, 10);
@@ -56,6 +32,8 @@ function AddNewTask() {
       navigate("/listOfTasks");
     } catch (error) {
       log("error", "There was a problem with creating the task:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -68,56 +46,61 @@ function AddNewTask() {
     navigate("/listOfTasks");
   };
 
-  const isDisabled =
-    !!titleError ||
-    !!descriptionError ||
-    title.length < 3 ||
-    description.length < 5;
+  
 
   return (
     <div className="w-screen h-screen bg-white overflow-y-scroll">
-      <div className="m-5 flex justify-between items-center">
-        <button
-          className="btn-sm bg-blue-500 text-white rounded"
-          onClick={handleListOfTask}
-        >
-          List Of Tasks
-        </button>
-        <div className="text-3xl font-bold text-gray-800 flex items-center justify-center">
-          Add New Task Page
+      {isLoading && (
+        <progress className="progress w-full absolute top-0"></progress>
+      )}
+      <div className="m-5 flex justify-between items-center lg:ml-[40vw]">
+        <div>
+          <h1 className="text-2xl text-gray-700 font-bold pl-12">
+            Welcome back,
+          </h1>
+          <p className="text-sm text-gray-700 font-semibold">
+            Please , Assign a new Task for the completion{" "}
+          </p>
         </div>
-        <button
-          className="btn-sm bg-red-500 text-white rounded"
-          onClick={handleLogout}
-        >
-          Logout
-        </button>
+       
+        <div>
+          <button
+            className="btn-sm bg-gray-500 font-semibold text-white rounded mx-2"
+            onClick={handleListOfTask}
+          >
+            List Of Tasks
+          </button>
+          <button
+            className="btn-sm bg-red-500 text-white rounded mx-2"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
       </div>
 
       <div className="flex items-center justify-center mt-4">
-        <div className="border shadow p-3 bg-white rounded pl-20 pr-20">
+        <div className="border shadow p-3 bg-white rounded pl-20 pr-20 ml-12">
           <div className="py-2">
-            <h2 className="text-3xl font-semibold text-gray-800">
+            <h2 className="text-xl font-semibold text-gray-800">
               Create a New Task
             </h2>
           </div>
           <form className="flex flex-col" onSubmit={handleSubmit}>
-            <label className="text-gray-800 font-medium py-2">
+            <label className="text-gray-800 font-medium py-2 text-sm">
               Enter Title
             </label>
             <input
               type="text"
               name="title"
               placeholder="Enter the Title"
-              className="p-2 border-gray-800 border rounded w-full max-w-xs bg-white text-black"
+              className="p-1 border-gray-800 border rounded w-full max-w-xs bg-white text-black"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
             />
-            {submitted && titleError && (
-              <div className="text-red-500 text-sm">{titleError}</div>
-            )}
+           
 
-            <label className="text-gray-800 font-medium py-2">
+            <label className="text-gray-800 font-medium py-2 text-sm">
               Enter Description
             </label>
             <textarea
@@ -129,11 +112,9 @@ function AddNewTask() {
               rows={7}
               cols={50}
             ></textarea>
-            {submitted && descriptionError && (
-              <div className="text-red-500 text-sm">{descriptionError}</div>
-            )}
+           
 
-            <label className="text-gray-800 font-medium py-2">
+            <label className="text-gray-800 font-medium py-2 text-sm">
               Select Status
             </label>
             <select
@@ -147,11 +128,11 @@ function AddNewTask() {
             </select>
 
             <button
-              className="btn-sm w-20 mt-5 bg-blue-500 text-white rounded"
+              className="text-xs  w-20 mt-3 py-1.5 bg-gray-500  text-white rounded"
               type="submit"
-              disabled={isDisabled}
+             
             >
-              Submit
+              Add Task
             </button>
           </form>
         </div>
--- todo_frontend/src/Components/ListOfTasks.tsx
+++ todo_frontend/src/Components/ListOfTasks.tsx
@@ -12,21 +12,38 @@ const statusLabels: { [key: string]: string } = {
   "2": "Done",
 };
 
+const statusColor = {
+  "0": "bg-red-500", 
+  "1": "bg-yellow-500",
+  "2": "bg-green-500", 
+};
+
+function getStatusColor(statusCode: "0" | "1" | "2"): string {
+  return statusColor[statusCode] || "bg-gray-500";
+}
+
 function ListOfTasks() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [error, setError] = useState<string | null>(null);
+
+ const [buttonClicked, setButtonClicked] = useState<Record<string, boolean>>(
+   {}
+ ); 
   const [selectedStatus, setSelectedStatus] = useState<string>("all");
+   const [expanded, setExpanded] = useState<Record<string, boolean>>({}); 
   const navigate = useNavigate();
+   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const loadTasks = async () => {
+         setIsLoading(true);  
       try {
         const fetchedTasks = await fetchTasks();
         setTasks(fetchedTasks);
         log("info", "Tasks fetched successfully:", fetchedTasks);
       } catch (error) {
         log("error", "There was a problem fetching tasks:", error);
-        setError("Failed to load tasks.");
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -45,34 +62,84 @@ function ListOfTasks() {
     localStorage.removeItem("accessToken");
     navigate("/");
   };
+ const handleCombinedClick = async (taskId: string) => {
+   setButtonClicked((prev) => ({ ...prev, [taskId]: !prev[taskId] }));
+   setIsLoading(true);
 
-const handleDelete = async (taskId: string) => {
-  try {
-    await deleteTask(taskId);
-    setTasks(tasks.filter((task) => task.id !== taskId));
-    log("info", "Task deleted successfully:", taskId); 
-  } catch (error) {
-    log("error", "There was a problem with the delete operation:", error);
-    setError("Failed to delete the task.");
-  }
-};
+   setTimeout(async () => {
+     try {
+       await deleteTask(taskId);
+       setTasks(tasks.filter((task) => task.id !== taskId));
+       log("info", "Task deleted successfully:", taskId);
+     } catch (error) {
+       log("error", "There was a problem with the delete operation:", error);
+     } finally {
+       setIsLoading(false); 
+       setButtonClicked((prev) => ({ ...prev, [taskId]: false }));
+     }
+
+   }, 1000); 
+ };
 
 
   const handleAddNewTask = () => {
     navigate("/addnewTask");
   };
+  const toggleDescription = (taskId: string) => {
+    setExpanded((prev) => ({ ...prev, [taskId]: !prev[taskId] }));
+  };
+const filteredTasks =
+  selectedStatus === "all"
+    ? tasks
+    : tasks.filter((task) => String(task.status) === selectedStatus);
 
-  const filteredTasks =
-    selectedStatus === "all"
-      ? tasks
-      : tasks.filter((task) => String(task.status) === selectedStatus);
 
   return (
-    <div className="w-screen h-screen bg-white overflow-y-scroll">
-      <div className="m-5 flex flex-col md:flex-row  justify-between items-center">
+    <div className="w-screen h-screen bg-[#f3f1f1] overflow-y-scroll flex flex-col relative">
+      {isLoading && (
+        <progress className="progress w-full absolute top-0"></progress>
+      )}
+      <div className="m-5 flex flex-col md:flex-row justify-between items-center lg:w-[96%] w-0  h-80 lg:h-10 ">
         <div>
+          <div className="pl-72 lg:pl-[38vw] relative">
+            <h1 className="text-xl lg:text-2xl text-gray-700 font-bold w-64 lg:pl-16">
+              Welcome back,
+            </h1>
+            <button
+              className=" bg-red-500 text-white rounded  ml-64 block lg:hidden p-0.5 absolute top-0 right-0"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+            <p className="text-sm text-gray-700 lg:pl-2 font-semibold">
+              You've got {filteredTasks.length} tasks coming up in the next
+              days.{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="w-1/4 flex hidden lg:block">
+          <button
+            className="btn-sm bg-red-500 text-white rounded font-semibold ml-64"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+
+      <div className=" items-center flex justify-between lg:w-[49vw] ml-[25vw]">
+        <div>
+          <button
+            className="btn-sm rounded-lg bg-gray-500 text-white opacity-80 font-medium lg:mx-2 hidden lg:block "
+            onClick={handleAddNewTask}
+          >
+            Add Task.. <i className="ri-add-line text-md"></i>
+          </button>
+        </div>
+        <div className="">
           <select
-            className="select select-bordered select-sm bg-white border border-gray-800 px-40  text-start rounded text-gray-800 pl-[8px]"
+            className="select select-bordered select-sm text-sm bg-white border border-gray-800   text-start rounded-lg text-gray-800 "
             value={selectedStatus}
             onChange={handleStatusChange}
           >
@@ -82,54 +149,60 @@ const handleDelete = async (taskId: string) => {
             <option value="2">Done</option>
           </select>
         </div>
-        <div className="text-gray-800 text-3xl font-bold">
-          <h1>List Of Task</h1>
-        </div>
-        <div>
-          <button
-            className="btn-sm bg-blue-500 text-white rounded lg:mx-2 "
-            onClick={handleAddNewTask}
-          >
-            Add New Task
-          </button>
-          <button
-            className="btn-sm bg-red-500 text-white rounded mx-2 "
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
-        </div>
       </div>
-      {error && <p className="text-red-500 text-center">{error}</p>}
       {filteredTasks.map((task, index) => (
-        <div key={index} className="border rounded shadow bg-white p-4 m-5">
-          <div className="flex justify-between items-center flex-col md:flex-row">
-            <div>
-              <h4 className="text-gray-700 font-semibold">
-                <span className="font-bold">Title :</span> {task.title}
-              </h4>
-              <p className="text-gray-700 font-medium">
-                <span className="font-bold">Description:</span>
-                {task.description}
-              </p>
-              <p className="text-gray-700 font-medium">
-                <span className="font-bold">Status:</span>{" "}
-                {statusLabels[String(task.status)]}
-              </p>
-            </div>
-            <div className="flex items-center">
-              <button
-                className="btn-sm bg-blue-500 text-white rounded mx-2"
-                onClick={() => handleEdit(task)}
-              >
-                Edit
-              </button>
-              <button
-                className="btn-sm bg-red-500 text-white rounded mx-2"
-                onClick={() => handleDelete(task.id)}
-              >
-                Delete
-              </button>
+        <div className="w-full items-center flex justify-center">
+          <div
+            key={index}
+            className="border rounded-lg shadow bg-white  lg:ml-4 ml-2 my-2 lg:w-[49vw] w-[75vw] pr-3 pl-4 pt-2 pb-2"
+          >
+            <div className="flex flex-col md:flex-col">
+              <div>
+                <h4 className="text-gray-700 font-semibold text-lg">
+                  {task.title}
+                </h4>
+                <p className="text-gray-700 text-sm pt-2">
+                  {expanded[task.id]
+                    ? task.description
+                    : `${task.description.split(" ").slice(0, 10).join(" ")} `}
+                  {task.description.split(" ").length > 30 && (
+                    <button
+                      onClick={() => toggleDescription(task.id)}
+                      className="text-blue-600 text-xs"
+                    >
+                      {expanded[task.id] ? "Read less.." : "Read more.."}
+                    </button>
+                  )}
+                </p>
+              </div>
+              <div className="flex">
+                <button
+                  className={` border border-0 border-gray-400 mt-2 text-white text-xs font-semibold rounded p-1 pl-2 pr-2 ${getStatusColor(
+                    task.status
+                  )}`}
+                  type="button"
+                >
+                  {statusLabels[task.status]}
+                </button>
+                <div className="flex ml-auto">
+                  <button
+                    className="text-[#9e9ea7] text-xl mx-2"
+                    onClick={() => handleEdit(task)}
+                  >
+                    <i className="ri-edit-line"></i>
+                  </button>
+
+                  <button
+                    key={task.id}
+                    className={`text-[#9e9ea7] ${
+                      buttonClicked[task.id] ? "text-2xl" : "text-xl"
+                    } mx-2`}
+                    onClick={() => handleCombinedClick(task.id)}
+                  >
+                    <i className="ri-delete-bin-line"></i>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
